a92ad3ac17fdc1d06c9cf0bf578e75af828d0a5e
Robert Hailey <rhailey@allogy.com>
2017-01-22 23:08:07 -0600

    b: logging

--- src/meta/works/zim/annotationhelper/LooksLike.java
+++ src/meta/works/zim/annotationhelper/LooksLike.java
@@ -1,14 +1,33 @@
 package meta.works.zim.annotationhelper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by robert on 2016-10-06 15:18.
  */
 public
 class LooksLike
 {
+	private static final
+	Logger log = LoggerFactory.getLogger(LooksLike.class);
+
 	public static
 	boolean videoFile(String url)
 	{
-		return url.endsWith(".mp4");
+		if (url==null || url.length()<5)
+		{
+			return false;
+		}
+
+		final
+		boolean retval=url.endsWith(".mp4");
+
+		final
+		char lastChar=url.charAt(url.length()-1);
+
+		log.debug("videoFile('{}')? {} (lastChar='{}')", url, retval, lastChar);
+
+		return retval;
 	}
 }
